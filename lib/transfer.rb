57c9d43fdac5e4e0f21871857eabb05ced9b10bc
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-07-19 18:29:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,11 @@
 class Transfer
   
-  attr_accessor :sender, :reciever, :amount
+  attr_accessor :sender, :receiver, :amount
   
   
-  def initialize(sender, reciever, amount)
+  def initialize(sender, receiver, amount)
     @sender = sender 
-    @reciever = reciever 
+    @receiver = reciever 
     @amount = amount 
   end 
   
