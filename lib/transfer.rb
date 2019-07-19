af83c899faf94d3ed588275757561fbefee4668a
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-07-19 18:40:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
     @status = "complete"
   else 
     @status = "rejected"
-    return ""
+    return "Transaction rejected. Please check your account balance."
     end 
   end 
   
